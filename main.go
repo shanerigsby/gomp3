f22f94ad46b9c4c45968d4b44c443062afeff17a
Shane Rigsby <shanerigsby@live.com>
2024-04-21 01:52:16 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"os"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"bytes"
+	"os/exec"
+	"regexp"
+	"path/filepath"
+	"net/url"
+	"encoding/json"
+)
+
+// sample config.json file:
+// {
+//     "port": "8100",
+//     "filesDir": "/home/user/mp3s",
+//     "execDir": "/home/user/downloads/yt-dlp",
+//     "outDir": "/home/user/mp3s"
+// }
+
+
+var filesDir string
+var execPath string
+var outDir string
+
+func main() {
+
+	configFilePath := flag.String("c", "config.json", "path to json config file")
+	flag.Parse()
+	config, err := getConfig(*configFilePath)
+	if err != nil {
+		log.Fatal("Error getting config: %v\n", err)
+	}
+
+	port := flag.String("p", config.Port, "port to serve on")
+	flag.StringVar(&filesDir, "d", config.FilesDir, "the directory where files are hosted")
+	flag.StringVar(&execPath, "e", config.ExecPath, "path of the yt-dlp executable")
+	flag.StringVar(&outDir, "o", config.OutDir, "directory to save mp3 output")
+	flag.Parse()
+
+	// serve files from the path specified by filesDir arg or default
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir(filesDir))))
+
+	// POST endpoint, expects body to be a string of a youtube url
+	http.HandleFunc("/mp3", handleMp3)
+
+	log.Printf("Serving %s on HTTP port: %s\n", filesDir, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
+func handleMp3(w http.ResponseWriter, r *http.Request) {
+	// Handle preflight OPTIONS request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading request body: %v", err), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	// body is expected to be a url string
+	url := string(body)
+	if url == "" {
+		http.Error(w, "URL not provided", http.StatusBadRequest)
+		return
+	}
+
+	if !isValidURL(url) {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+
+	// videoID is used as the mp3 filename
+	videoID := getVideoID(url)
+	if (videoID == "") {
+		http.Error(w, "Bad YT URL", http.StatusBadRequest)
+		return
+	}
+
+	if (fileAlreadyExists(videoID)) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, fmt.Sprintf("%s/%s/%s.mp3", r.Host, "files", videoID))
+		return
+	}
+
+	// call the yt-dlp executable with audio only mp3 options
+	outfile := filepath.Join(outDir, fmt.Sprintf("%s.mp3", videoID))
+	cmdArgs := []string{url, "-x", "--audio-format", "mp3", "-o", outfile}
+	cmd := exec.Command(execPath, cmdArgs...)
+	var out bytes.Buffer
+    cmd.Stdout = &out
+    err = cmd.Run()
+
+    if err != nil {
+        http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf(out.String())
+		return
+    }
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, fmt.Sprintf("%s/%s/%s.mp3", r.Host, "files", videoID))
+}
+
+func isValidURL(url string) bool {
+	regex := regexp.MustCompile(`^(http|https)://[^ "]+$`)
+	return regex.MatchString(url)
+}
+
+func getVideoID(inputURL string) string {
+	urlPattern := regexp.MustCompile(`^https://youtu\.be/([a-zA-Z0-9_-]+)$`)
+	match := urlPattern.FindStringSubmatch(inputURL)
+
+	if len(match) > 1 {
+		videoID := match[1]
+		log.Printf("Video ID: %s", videoID)
+		return videoID
+	}
+
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		log.Printf("Error parsing URL: %v", err)
+		return ""
+	}
+
+	videoID := parsedURL.Query().Get("v")
+	if videoID != "" {
+		return videoID
+	}
+
+	return ""
+}
+
+func fileAlreadyExists(videoID string) bool {
+	mp3Path := filepath.Join(filesDir, fmt.Sprintf("%s.mp3", videoID))
+
+	if _, err := os.Stat(mp3Path); os.IsNotExist(err) {
+		fmt.Printf("File %s does not exist.\n", mp3Path)
+		return false
+	} else if err != nil {
+		fmt.Printf("Error checking file %s: %v\n", mp3Path, err)
+		return false
+	} else {
+		fmt.Printf("File %s exists.\n", mp3Path)
+		return true
+	}
+}
+
+type Config struct {
+	Port     string `json:"port"`
+	FilesDir string `json:"filesDir"`
+	ExecPath  string `json:"execDir"`
+	OutDir   string `json:"outDir"`
+}
+
+func getConfig(configFilePath string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return config, fmt.Errorf("error opening config file: %v", err)
+	}
+	defer configFile.Close()
+
+	err = json.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		return config, fmt.Errorf("error decoding config file: %v", err)
+	}
+
+	return config, nil
+}
